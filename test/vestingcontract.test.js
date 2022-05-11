47cda853799c3bf855e097bebe19be2215b73486
Jeet Patel <jp2398d@gmail.com>
2022-05-11 18:20:04 +0530

    modified a few tests

--- test/vestingcontract.test.js
+++ test/vestingcontract.test.js
@@ -4,18 +4,11 @@ const { BigNumber } = require('ethers');
 
 let VestingContract;
 let hardhatToken;
-let totalBalance;
 let owner;
 let advisor;
 let partner;
 let mentor;
 let addrs;
-let advisorAccount;
-let advisorBalance;
-let partnerAccount;
-let partnerBalance;
-let mentorAccount;
-let mentorBalance;
 
 beforeEach(async function() {
     [owner, advisor, partner, mentor, ...addrs] = await ethers.getSigners();
@@ -49,16 +42,8 @@ describe('Cliff period terminated', function () {
     it('advisor balance 1 month from cliff period termination is 227,272.727 tokens', async function() {
         const threeMonthsLater = 92 * 24 * 60 * 60;
 
-        const blockNumBefore = await ethers.provider.getBlockNumber();
-        const blockBefore = await ethers.provider.getBlock(blockNumBefore);
-        const timestampBefore = blockBefore.timestamp;
-
         await ethers.provider.send('evm_increaseTime', [threeMonthsLater]);
         await ethers.provider.send('evm_mine');
-
-        const blockNumAfter = await ethers.provider.getBlockNumber();
-        const blockAfter = await ethers.provider.getBlock(blockNumAfter);
-        const timestampAfter = blockAfter.timestamp;
       
         await hardhatToken.connect(advisor).withdrawToken();
         let testingVar = await hardhatToken.balanceOf(advisor.address);
@@ -70,16 +55,8 @@ describe('22 months linear vesting completed', function () {
     it('total tokens with the advisor should be 5 million ', async function () {      
         const vestingPeriodCompleted = 721 * 24 * 60 * 60;
 
-        const blockNumBefore = await ethers.provider.getBlockNumber();
-        const blockBefore = await ethers.provider.getBlock(blockNumBefore);
-        const timestampBefore = blockBefore.timestamp;
-
         await ethers.provider.send('evm_increaseTime', [vestingPeriodCompleted]);
         await ethers.provider.send('evm_mine');
-
-        const blockNumAfter = await ethers.provider.getBlockNumber();
-        const blockAfter = await ethers.provider.getBlock(blockNumAfter);
-        const timestampAfter = blockAfter.timestamp;
       
         await hardhatToken.connect(advisor).withdrawToken();
         let testingVar = await hardhatToken.balanceOf(advisor.address);
